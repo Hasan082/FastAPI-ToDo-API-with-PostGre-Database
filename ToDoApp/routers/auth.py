34fca3e882f8683446673ce0634b9058e8a2c3ed
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-21 00:03:24 +0600

    [ADDED] Database connection done fro user create

--- ToDoApp/routers/auth.py
+++ ToDoApp/routers/auth.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from database import SessionLocal
 from models import User
+from passlib.context import CryptContext
 
 router = APIRouter()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class UserRequest(BaseModel):
@@ -14,15 +22,33 @@ class UserRequest(BaseModel):
     role: str
 
 
+def get_db():
+    """
+    Dependency function that provides a database session.
+    The session is yielded to the endpoint and closed automatically after the request is completed.
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# Creating dependency variable to avoid duplicate use
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
 @router.post("/auth")
-async def create_user(create_user_request: UserRequest):
+async def create_user(db: db_dependency, create_user_request: UserRequest):
     create_user_model = User(
         email=create_user_request.email,
         username=create_user_request.username,
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
         role=create_user_request.role,
-        hashed_password=create_user_request.password,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True
     )
-    return create_user_model
+
+    db.add(create_user_model)
+    db.commit()
