e8082dd207470151f4ba33ff8560cfa9f95169b0
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-21 23:32:57 +0600

    [ADDED] Added jwt library

--- ToDoApp/routers/auth.py
+++ ToDoApp/routers/auth.py
@@ -1,12 +1,13 @@
 from typing import Annotated
-
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-
+from starlette import status
 from database import SessionLocal
 from models import User
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
 
 router = APIRouter()
 
@@ -38,7 +39,16 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@router.post("/auth")
+def authenticate_user(username: str, password: str, db):
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: UserRequest):
     create_user_model = User(
         email=create_user_request.email,
@@ -52,3 +62,12 @@ async def create_user(db: db_dependency, create_user_request: UserRequest):
 
     db.add(create_user_model)
     db.commit()
+
+
+@router.post("/token")
+async def create_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Failed to authenticate user'
+    return 'Authentication is successful.'
+
