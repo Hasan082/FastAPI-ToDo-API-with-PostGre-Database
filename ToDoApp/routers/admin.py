bf610f78e437d0b6763f0cd3fbebb479549b5cde
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-22 02:56:26 +0600

    [ADDED] Admin router delete end point created

--- ToDoApp/routers/admin.py
+++ ToDoApp/routers/admin.py
@@ -36,3 +36,14 @@ async def read_all_todos(user: user_dependency, db: db_dependency):
     if user is None or user.get('user_role') != 'admin':
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Not authenticated')
     return db.query(ToDos).all()
+
+
+@router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
+    if user is None or user.get('user_role') != 'admin':
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Not authenticated')
+    todo_model = db.query(ToDos).filter(ToDos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='ToDo not found')
+    db.query(ToDos).filter(ToDos.id == todo_id).delete()
+    db.commit()
