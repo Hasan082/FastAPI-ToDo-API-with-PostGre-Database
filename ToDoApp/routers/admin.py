3d0e04a58c8775a3519f8ee0869ec5ce57115ada
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-22 17:37:28 +0600

    [UPDATE] Write comment for users.py

--- ToDoApp/routers/admin.py
+++ ToDoApp/routers/admin.py
@@ -5,8 +5,9 @@ from models import ToDos  # Importing the ToDos model from the models module
 from database import SessionLocal  # Importing the engine and SessionLocal for database interaction
 from starlette import status  # Importing status codes from starlette
 from pydantic import BaseModel, Field
-from .auth import get_current_user
+from .auth import get_current_user  # Importing the get_current_user function from auth module
 
+# Creating an API router for admin-specific routes with a prefix and tags
 router = APIRouter(
     prefix='/admin',
     tags=['admin']
@@ -26,13 +27,23 @@ def get_db():
         db.close()
 
 
-# Creating dependency variable to avoid duplicate use
+# Creating dependency variables to avoid duplicate use
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
 @router.get("/todo", status_code=status.HTTP_200_OK)
 async def read_all_todos(user: user_dependency, db: db_dependency):
+    """
+    Endpoint to read all ToDos for admin users.
+
+    Parameters:
+    - `user`: The current authenticated user with admin privileges.
+    - `db`: Dependency to obtain a database session.
+
+    Returns a list of all ToDo items if the user is authenticated and has admin privileges.
+    Raises a 401 HTTPException if the user is not authenticated or is not an admin.
+    """
     if user is None or user.get('user_role') != 'admin':
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Not authenticated')
     return db.query(ToDos).all()
@@ -40,13 +51,25 @@ async def read_all_todos(user: user_dependency, db: db_dependency):
 
 @router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_todo(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
+    """
+    Endpoint to delete a specific ToDo item by its ID for admin users.
+
+    Parameters:
+    - `user`: The current authenticated user with admin privileges.
+    - `db`: Dependency to obtain a database session.
+    - `todo_id`: Path parameter representing the ID of the ToDo item to delete, must be greater than 0.
+
+    Deletes the ToDo item identified by `todo_id` if the user is authenticated and has admin privileges.
+    Raises a 401 HTTPException if the user is not authenticated or is not an admin.
+    Raises a 404 HTTPException if the ToDo item is not found.
+    """
     if user is None or user.get('user_role') != 'admin':
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Not authenticated')
+
     todo_model = db.query(ToDos).filter(ToDos.id == todo_id).first()
+
     if todo_model is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='ToDo not found')
+
     db.query(ToDos).filter(ToDos.id == todo_id).delete()
     db.commit()
-
-
-# https://gale.udemy.com/course/fastapi-the-complete-course/learn/lecture/37437346#overview
--- ToDoApp/routers/users.py
+++ ToDoApp/routers/users.py
@@ -2,17 +2,17 @@ from typing import Annotated  # Importing Annotated for type annotations with me
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session  # Importing Session for type hinting
 from fastapi import APIRouter, Depends, HTTPException, Path  # Importing FastAPI, Depends for dependency injection
-from models import ToDos, User  # Importing the ToDos model from the models module
+from models import ToDos, User  # Importing the ToDos and User models from the models module
 from database import SessionLocal  # Importing the engine and SessionLocal for database interaction
 from starlette import status  # Importing status codes from starlette
 from pydantic import BaseModel, Field
-from .auth import get_current_user
+from .auth import get_current_user  # Importing the get_current_user function from auth module
 
+# Creating an API router for user-specific routes with a prefix and tags
 router = APIRouter(
     prefix='/users',
     tags=['users']
 )
-
 
 # Dependency function to get a database session
 def get_db():
@@ -26,36 +26,67 @@ def get_db():
     finally:
         db.close()
 
-
-class UserVerification(BaseModel):
-    password: str
-    new_password: str
-
-
-# Creating dependency variable to avoid duplicate use
+# Creating dependency variables to avoid duplicate use
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+# Pydantic model for password verification and update
+class UserVerification(BaseModel):
+    """
+    Pydantic model for verifying and updating user password.
+
+    Attributes:
+        password (str): The current user password.
+        new_password (str): The new password to be set.
+    """
+    password: str
+    new_password: str
 
 @router.get("/me", status_code=status.HTTP_200_OK)
 async def read_user(user: user_dependency, db: db_dependency):
+    """
+    Endpoint to fetch details of the currently authenticated user.
+
+    Parameters:
+    - `user`: The current authenticated user obtained from the request dependency.
+    - `db`: Dependency to obtain a database session.
+
+    Returns:
+    - Returns the details of the current user if authenticated.
+    - Raises HTTP 401 Unauthorized if the user is not authenticated.
+    """
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
     return db.query(User).filter(User.id == user.get('user_id')).first()
 
-
 @router.put("/me/password", status_code=status.HTTP_204_NO_CONTENT)
 async def update_password(user: user_dependency, db: db_dependency, userVerification: UserVerification):
+    """
+    Endpoint to update the password of the currently authenticated user.
 
+    Parameters:
+    - `user`: The current authenticated user obtained from the request dependency.
+    - `db`: Dependency to obtain a database session.
+    - `userVerification`: Request body containing the current password and new password.
+
+    Updates the password of the authenticated user if the current password matches.
+    Raises HTTP 401 Unauthorized if the user is not authenticated or the current password is incorrect.
+
+    Notes:
+    - Passwords are hashed using bcrypt for security.
+    """
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
+
     user_model = db.query(User).filter(User.id == user.get('user_id')).first()
 
     if not bcrypt_context.verify(userVerification.password, user_model.hashed_password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password")
 
+    # Hash the new password before updating
     user_model.hashed_password = bcrypt_context.hash(userVerification.new_password)
 
+    # Add the updated user model to the database session and commit the transaction
     db.add(user_model)
     db.commit()
